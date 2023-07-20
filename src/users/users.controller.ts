6f45be87f4c2d1a071bb8a8c9f00d3e184385dce
Vladimir Staninov <52317262+cantona94@users.noreply.github.com>
2023-07-20 17:14:34 +0500

    add JwtAuthGuard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards } from '@nestjs/common';
 import { createUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
+import { JwtAuthGuard } from 'src/auth/jwt-auth-guard';
 
 @ApiTags('Пользователи')
 @Controller('users')
@@ -19,6 +20,7 @@ export class UsersController {
 
   @ApiOperation({ summary: 'Получение всех поьзователей' })
   @ApiResponse({ status: 200, type: [User] })
+  @UseGuards(JwtAuthGuard)
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
