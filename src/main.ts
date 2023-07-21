14ea5475ec746c0f27bd5c908bfa05946fb3d608
Vladimir Staninov <52317262+cantona94@users.noreply.github.com>
2023-07-21 21:11:47 +0500

    add validation.pipe

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-
+import { ValidationPipe } from "./pipes/validation.pipe";
 
 async function start() {
   const PORT = process.env.PORT || 5000;
@@ -15,7 +15,9 @@ async function start() {
     .build()
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api/docs', app, document)
+  SwaggerModule.setup('/api/docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(PORT, () => console.log(`server started on port = ${PORT}`))
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class createUserDto {
 
   @ApiProperty({ example: 'user@mail.ru', description: 'Почтовый адрес' })
+  @IsString({ message: 'Должно быть строкой' })
+  @IsEmail({}, { message: "Некорректный email" })
   readonly email: string;
-
+  @IsString({ message: 'Должно быть строкой' })
+  @Length(4, 16, { message: 'Не меньше 4 и не больше 16' })
   @ApiProperty({ example: '123', description: 'Пароль' })
   readonly password: string;
 }
